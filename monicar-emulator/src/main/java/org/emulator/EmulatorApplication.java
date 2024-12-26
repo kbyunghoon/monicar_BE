fa1c1eeb598dd80cee3dbb86be913feff81d4bf0
Suxxxxhyun <qkrtngus116@naver.com>
2024-12-26 09:41:47 +0900

    build: 공통모듈세팅

--- monicar-emulator/src/main/java/org/emulator/EmulatorApplication.java
+++ monicar-emulator/src/main/java/org/emulator/EmulatorApplication.java
@@ -1,10 +1,28 @@
 package org.emulator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.common.TestBean;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication(scanBasePackages = {"org.emulator", "org.common"})
 public class EmulatorApplication {
+
+    private final TestBean testBean;
+
+    @Autowired
+    public EmulatorApplication(TestBean testBean) {
+        this.testBean = testBean;
+    }
+
+    @PostConstruct
+    public void dependencyTest() {
+        testBean.dependencyTest();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EmulatorApplication.class, args);
     }
