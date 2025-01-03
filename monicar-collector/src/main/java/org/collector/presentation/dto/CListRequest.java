a68d64f85e281ee5302ee2c3c00c623b446aa12e
Suxxxxhyun <qkrtngus116@naver.com>
2025-01-03 16:45:41 +0900

    refactor: interval_at -> 초정보까지 받도록 변경

--- monicar-collector/src/main/java/org/collector/presentation/dto/CListRequest.java
+++ monicar-collector/src/main/java/org/collector/presentation/dto/CListRequest.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record CListRequest(
 	@NotNull(message = "발생시간은 필수 입력값입니다.")
-	@JsonFormat(pattern = "yyyyMMddHHmm")
+	@JsonFormat(pattern = "yyyyMMddHHmmss")
 	LocalDateTime interval_at,
 
 	@NotNull(message = "유효하지 않은 GPS상태 값이 입력되었습니다.")
