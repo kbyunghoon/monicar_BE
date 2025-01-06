5a755ea227eb27ba76aee61b8c3c7f86d95c867a
Suxxxxhyun <qkrtngus116@naver.com>
2025-01-06 15:42:12 +0900

    refactor: 주기정보 저장시, 유효성검사에 실패하면 올바르게 mdn정보를 반환하도록 수정

--- monicar-collector/src/main/java/org/collector/application/CycleInfoService.java
+++ monicar-collector/src/main/java/org/collector/application/CycleInfoService.java
@@ -2,11 +2,12 @@ package org.collector.application;
 
 import java.util.List;
 
+import org.collector.common.exception.CustomException;
+import org.collector.common.response.ResponseCode;
 import org.collector.domain.CycleInfo;
 import org.collector.domain.VehicleInformation;
 import org.collector.infrastructure.repository.CycleInfoRepository;
 import org.collector.infrastructure.repository.VehicleInformationRepository;
-import org.collector.presentation.dto.CListRequest;
 import org.collector.presentation.dto.CycleInfoRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,22 +15,23 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CycleInfoService {
 	private final CycleInfoRepository cycleInfoRepository;
 	private final VehicleInformationRepository vehicleInformationRepository;
 
-	@Transactional
 	public long cycleInfoSave(final CycleInfoRequest request) {
-		VehicleInformation vehicleInformation = vehicleInformationRepository.findByMdn(request.mdn())
-			.orElseThrow(IllegalArgumentException::new);
+		if(!vehicleInformationRepository.existsByMdn(request.mdn())){
+			throw new CustomException(ResponseCode.ENTITY_NOT_FOUND);
+		}
 
-		CycleInfoRequest.from(request);
+		VehicleInformation vehicleInformation = VehicleInformation.from(request);
 
 		vehicleInformationRepository.save(vehicleInformation);
 
 		List<CycleInfo> cycleInfos = request.cList().stream()
-			.map(cListRequest -> CListRequest.from(cListRequest, vehicleInformation))
+			.map(cListRequest -> CycleInfo.from(cListRequest, vehicleInformation))
 			.toList();
 
 		cycleInfoRepository.saveAll(cycleInfos);
--- monicar-collector/src/main/java/org/collector/infrastructure/repository/VehicleInformationRepository.java
+++ monicar-collector/src/main/java/org/collector/infrastructure/repository/VehicleInformationRepository.java
@@ -1,10 +1,8 @@
 package org.collector.infrastructure.repository;
 
-import java.util.Optional;
-
 import org.collector.domain.VehicleInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleInformationRepository extends JpaRepository<VehicleInformation, Long> {
-	Optional<VehicleInformation> findByMdn(Long mdn);
+	boolean existsByMdn(Long mdn);
 }
