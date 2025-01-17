7eb1bb7e13fbd0eb9d9f9d48acf8e18a8bf283d9
jeongjihun <wlgns2234@naver.com>
2025-01-18 01:35:45 +0900

    feat: 홈화면에서 on 요청하도록 구현

--- monicar-emulator/src/main/java/org/emulator/device/presentation/VehicleApiController.java
+++ monicar-emulator/src/main/java/org/emulator/device/presentation/VehicleApiController.java
@@ -1,13 +1,12 @@
 package org.emulator.device.presentation;
 
-import lombok.RequiredArgsConstructor;
-
-import org.common.dto.CommonResponse;
 import org.emulator.device.application.VehicleService;
 import org.emulator.device.common.response.BaseResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,11 +16,6 @@ public class VehicleApiController {
 
 	@PostMapping("/key-on")
 	public BaseResponse<Void> keyOn() {
-		CommonResponse response = vehicleService.onVehicle();
-
-		if (!response.rstCd().equals("000"))
-			return BaseResponse.fail(response);
-
-		return BaseResponse.success();
+		return vehicleService.onVehicle();
 	}
 }
