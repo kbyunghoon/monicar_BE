98f9d09ac9e4a7c806a337e8e7ad5b3e45982101
kbyunghoon <dev.kbhoon@gmail.com>
2025-01-30 23:55:17 +0900

    chore: 오류 확인을 위한 오류 로그 추가

--- monicar-control-center/src/main/java/org/controlcenter/company/presentation/AuthController.java
+++ monicar-control-center/src/main/java/org/controlcenter/company/presentation/AuthController.java
@@ -1,5 +1,7 @@
 package org.controlcenter.company.presentation;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.controlcenter.common.exception.BusinessException;
 import org.controlcenter.common.response.BaseResponse;
 import org.controlcenter.common.response.code.ErrorCode;
@@ -18,6 +20,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -50,6 +53,7 @@ public class AuthController {
 			return ResponseEntity.ok(BaseResponse.success());
 
 		} catch (BusinessException | JwtException e) {
+			log.error(e.getMessage(), e);
 			return createErrorResponse();
 		}
 	}
@@ -71,6 +75,7 @@ public class AuthController {
 			return ResponseEntity.ok(BaseResponse.success("새로운 Refresh Token 발급 완료"));
 
 		} catch (BusinessException | JwtException e) {
+			log.error(e.getMessage(), e);
 			return createErrorResponse();
 		}
 	}
