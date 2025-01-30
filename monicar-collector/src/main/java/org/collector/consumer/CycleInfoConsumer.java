2e397770b15c982ebc6e40baeaf47db9b1a4886d
Suxxxxhyun <qkrtngus116@naver.com>
2025-01-30 17:56:52 +0900

    fix: 파티션 여러개 분리하기 위해 설정작업 변경

--- monicar-collector/src/main/java/org/collector/consumer/CycleInfoConsumer.java
+++ monicar-collector/src/main/java/org/collector/consumer/CycleInfoConsumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.collector.application.CycleInfoService;
 import org.collector.presentation.dto.CycleInfoRequest;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -13,10 +14,8 @@ import lombok.RequiredArgsConstructor;
 public class CycleInfoConsumer {
 	private final CycleInfoService cycleInfoService;
 
-	@KafkaListener(
-		topics = { "cycleInfo-json-topic" },
-		groupId = "consumer-group"
-	)
+	@KafkaListener(topicPartitions = @TopicPartition(topic = "cycleInfo-json-topic",
+		partitions = "#{@finder.partitions('cycleInfo-json-topic')}"))
 	public void accept(ConsumerRecord<String, CycleInfoRequest> message) {
 		System.out.println("[Main Consumer] Message arrived! - " + message.value());
 		cycleInfoService.cycleInfoSave(message.value());
