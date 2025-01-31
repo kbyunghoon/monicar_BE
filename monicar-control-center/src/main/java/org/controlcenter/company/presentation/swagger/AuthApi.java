124801a31c4fe7220c48673ac8cc3ef9209bffa1
kbyunghoon <dev.kbhoon@gmail.com>
2025-01-31 17:07:19 +0900

    docs: 토큰 관련 API 문서화 추가

--- monicar-control-center/src/main/java/org/controlcenter/company/presentation/swagger/AuthApi.java
+++ monicar-control-center/src/main/java/org/controlcenter/company/presentation/swagger/AuthApi.java
@@ -1,2 +1,31 @@
-package org.controlcenter.company.presentation.swagger;public class AuthApi {
+package org.controlcenter.company.presentation.swagger;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Tag(name = "토큰 재발급 API", description = "사용자 관련 토큰 재발급 기능 API")
+public interface AuthApi {
+
+	@Operation(summary = "액세스 토큰 재발급 API",
+		description = "리프레시 토큰으로 액세스 토큰 재발급")
+	@PostMapping("/logout")
+	ResponseEntity<?> refreshAccessToken(
+		@Parameter(hidden = true) @CookieValue(value = "access_token", required = false) String accessToken,
+		@Parameter(hidden = true) @CookieValue("refresh_token") String refreshToken,
+		HttpServletResponse response
+	);
+
+	@Operation(summary = "리프레시 토큰 재발급 API",
+		description = "액세스 토큰으로 리프레시 토큰 재발급")
+	ResponseEntity<?> reissueRefreshToken(
+		@Parameter(hidden = true) @CookieValue("access_token") String accessToken,
+		@Parameter(hidden = true) @CookieValue(value = "refresh_token", required = false) String oldRefreshToken,
+		HttpServletResponse response
+	);
 }
