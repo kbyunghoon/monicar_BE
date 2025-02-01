7776877ff45b63888c94e562ee4fe26b54d41a80
tomatozil <tomatoziiilll@gmail.com>
2025-02-02 02:20:03 +0900

    feat: SensorDetector 구현체 갈아끼우기

--- monicar-emulator/src/main/java/org/emulator/sensor/GpsDetector.java
+++ monicar-emulator/src/main/java/org/emulator/sensor/GpsDetector.java
@@ -1,13 +1,11 @@
 package org.emulator.sensor;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import java.util.concurrent.LinkedBlockingDeque;
-
-import org.emulator.sensor.dto.Gps;
+import org.emulator.device.application.port.EmulatorRepository;
+import org.emulator.sensor.dto.GpsListCircular;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,11 +14,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GpsDetector implements SensorDetector {
 	private final LocationHolder locationHolder;
+	private final EmulatorRepository emulatorRepository;
+	private final GpxFileParser gpxFileParser;
+	private final GpsListCircular gpsListCircular;
+
+	@PostConstruct
+	public void loadFile() {
+		gpsListCircular.loadFileData(gpxFileParser.parse());
+	}
 
 	@Scheduled(initialDelay = 1000, fixedDelay = 1000)
 	@Override
 	public void detect() {
 		log.info("[Thread: {}] {}", Thread.currentThread().getName(), "Detecting... GPS");
-		locationHolder.addGpsData(new Gps(20.111111, 30.111111));
+		if (emulatorRepository.isTurnOn()) {
+			locationHolder.addGpsData(gpsListCircular.getMovingGps());
+		} else {
+			locationHolder.addGpsData(gpsListCircular.getStopGps());
+		}
 	}
 }
---
+++ monicar-emulator/src/main/java/org/emulator/sensor/dto/GpsListCircular.java
@@ -0,0 +1,46 @@
+package org.emulator.sensor.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GpsListCircular {
+	private final List<Gps> gpsList;
+	private int gpsListSize;
+	private int interruptedIdx;
+
+	public GpsListCircular() {
+		gpsList = new ArrayList<>();
+		gpsListSize = 0;
+		interruptedIdx = 0;
+
+		gpsList.add(new Gps(20.111111, 30.111111));
+	}
+
+	public void loadFileData(Queue<Gps> fileData) {
+		gpsList.clear();
+		gpsList.addAll(fileData);
+		gpsListSize = gpsList.size();
+	}
+
+	public void add(Gps gps) {
+		gpsList.add(gps);
+	}
+
+	public Gps getMovingGps() {
+		Gps data = gpsList.get(interruptedIdx);
+		interruptedIdx = getInfiniteIdx();
+		return data;
+	}
+
+	private int getInfiniteIdx() {
+		return (interruptedIdx + 1) % gpsListSize;
+	}
+
+	public Gps getStopGps() {
+		return gpsList.get(interruptedIdx);
+	}
+}
