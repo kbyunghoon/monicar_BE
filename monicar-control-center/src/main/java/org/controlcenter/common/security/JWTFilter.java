50859578079673e67b6d5e92d73140b0afe32cb5
kbyunghoon <dev.kbhoon@gmail.com>
2025-02-03 15:12:34 +0900

    chore: 사용자 mock 계정 추가

--- monicar-control-center/src/main/java/org/controlcenter/common/security/JWTFilter.java
+++ monicar-control-center/src/main/java/org/controlcenter/common/security/JWTFilter.java
@@ -29,7 +29,6 @@ public class JWTFilter extends OncePerRequestFilter {
 	private static final List<String> EXCLUDED_URIS = List.of(
 		"/auth/refresh",
 		"/api/v1/sign-in",
-		"/api/v1/sign-up",
 		"/auth/reissue",
 		"/api/v1/logout",
 		"/swagger-ui/index.html"
--- monicar-control-center/src/main/resources/data/init-data.sql
+++ monicar-control-center/src/main/resources/data/init-data.sql
@@ -106,6 +106,8 @@ VALUES (1, 1, 'john@example.com', '김난아', 'password', '회장', 'ROLE_ADMIN
        (7, 7, 'kevin@example.com', '정지훈', 'password', '주임', 'ROLE_USER', '2025-01-07 15:30:00',
         '2025-01-07 15:00:00', '2025-01-07 15:00:00', NULL),
        (8, 8, 'lucy@example.com', '박수현', 'password', '차장', 'ROLE_ADMIN', '2025-01-08 16:30:00',
+        '2025-01-08 16:00:00', '2025-01-08 16:00:00', NULL),
+       (9, 1, 'test@test.com', 'string', '$2a$10$qzMmZdAe/mqroLeffNs4b.kW6qQaRF28oHwdVmyHgcxXbphPfZBca', '테스트', 'ROLE_ADMIN', '2025-01-08 16:30:00',
         '2025-01-08 16:00:00', '2025-01-08 16:00:00', NULL);
 
 
