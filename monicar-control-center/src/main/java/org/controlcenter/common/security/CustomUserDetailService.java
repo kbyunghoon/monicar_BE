eb2adc4db73386c05af40532e49b084d96d6c0c5
kbyunghoon <dev.kbhoon@gmail.com>
2025-02-03 15:11:15 +0900

    fix: 로그인 아이디 유효성 검사 롤백(유효성 검사 불필요)

--- monicar-control-center/src/main/java/org/controlcenter/common/security/CustomUserDetailService.java
+++ monicar-control-center/src/main/java/org/controlcenter/common/security/CustomUserDetailService.java
@@ -1,11 +1,9 @@
 package org.controlcenter.common.security;
 
-import java.util.regex.Pattern;
-
+import org.controlcenter.common.exception.BusinessException;
 import org.controlcenter.common.response.code.ErrorCode;
 import org.controlcenter.company.infrastructure.jpa.ManagerJpaRepository;
 import org.controlcenter.company.infrastructure.jpa.entity.ManagerEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,18 +14,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
-	private static final String REGEX_USER_ID = "^[A-Za-z0-9]+$";
-
 	private final ManagerJpaRepository managerJpaRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String loginId) throws AuthenticationException {
-		if (!Pattern.matches(REGEX_USER_ID, loginId)) {
-			throw new BadCredentialsException(ErrorCode.INVALID_TYPE_USERID_VALUE.getMessage());
-		}
-
 		ManagerEntity managerEntity = managerJpaRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new BadCredentialsException(ErrorCode.LOGIN_FAILED.getMessage()));
+			.orElseThrow(() -> new BusinessException(ErrorCode.LOGIN_FAILED));
 
 		return new CustomUserDetails(managerEntity.toDomain());
 	}
