f9edaf7f9e5a1b7b7186d7f5b155fd6d3bfbd1c6
Suxxxxhyun <qkrtngus116@naver.com>
2025-02-08 11:42:25 +0900

    fix: 요청받은 데이터의 주기정보 개수가 60개인지 확인 -> 60개가 아니라면 예외처리

--- monicar-collector/src/main/java/org/collector/consumer/CycleInfoConsumer.java
+++ monicar-collector/src/main/java/org/collector/consumer/CycleInfoConsumer.java
@@ -2,13 +2,18 @@ package org.collector.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.collector.application.CycleInfoService;
+import org.collector.common.constant.CycleInfoSize;
+import org.collector.common.exception.CustomException;
+import org.collector.common.response.ResponseCode;
 import org.collector.presentation.dto.CycleInfoRequest;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class CycleInfoConsumer {
@@ -17,6 +22,9 @@ public class CycleInfoConsumer {
 	@KafkaListener(topicPartitions = @TopicPartition(topic = "cycleInfo-json-topic",
 		partitions = "#{@finder.partitions('cycleInfo-json-topic')}"))
 	public void accept(ConsumerRecord<String, CycleInfoRequest> message) {
+		if(message.value().cList().size() != CycleInfoSize.CYCLE_INFO_MAX_SIZE.getSize()){
+			throw new CustomException(ResponseCode.NOT_CYCLE_INFO_SIZE_ERROR);
+		}
 		System.out.println("[Main Consumer] Message arrived! - " + message.value());
 		cycleInfoService.cycleInfoSave(message.value());
 	}
--- monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
+++ monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
@@ -1,7 +1,10 @@
 package org.collector.presentation;
 
 import org.collector.application.CycleInfoService;
+import org.collector.common.constant.CycleInfoSize;
+import org.collector.common.exception.CustomException;
 import org.collector.common.response.CommonResponse;
+import org.collector.common.response.ResponseCode;
 import org.collector.presentation.dto.CycleInfoRequest;
 import org.collector.producer.CycleInfoProducer;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +23,13 @@ public class CycleInfoController {
 	private final CycleInfoProducer cycleInfoProducer;
 	private final CycleInfoService cycleInfoService;
 
-	// FIXME: 테스트를 위한 코드. 추후 수정 필요.
 	@PostMapping
 	public CommonResponse<Void> cycleInfoSave(final @Valid @RequestBody CycleInfoRequest request) {
-		long mdn = cycleInfoService.cycleInfoSave(request);
+		if(request.cList().size() != CycleInfoSize.CYCLE_INFO_MAX_SIZE.getSize()){
+			throw new CustomException(ResponseCode.NOT_CYCLE_INFO_SIZE_ERROR);
+		}
+
+		Long mdn = cycleInfoService.cycleInfoSave(request);
 		cycleInfoProducer.sendMessage(request);
 		return CommonResponse.ok(mdn);
 	}
