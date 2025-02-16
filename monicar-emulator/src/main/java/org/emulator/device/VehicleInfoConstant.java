83adf3d0cbd3c40ffcaf48a91e9ce7cf82dc4694
tomatozil <tomatoziiilll@gmail.com>
2025-02-16 18:58:30 +0900

    feat: profile마다 차량 고정값 다르게 쓰기

--- monicar-emulator/src/main/java/org/emulator/device/VehicleInfoConstant.java
+++ monicar-emulator/src/main/java/org/emulator/device/VehicleInfoConstant.java
@@ -3,9 +3,7 @@ package org.emulator.device;
 import org.emulator.device.common.exception.BusinessException;
 import org.emulator.device.common.response.ErrorCode;
 
-public final class VehicleConstant {
-	public static final long VEHICLE_PRIMARY_KEY = 1234567890L;
-	public static final String VEHICLE_NUMBER = "123나1234";
+public class VehicleInfoConstant {
 	public static final String TERMINAL_ID = "TID001";
 	public static final long MANUFACTURER_ID = 1L;
 	public static final int PACKET_VERSION = 1;
@@ -15,7 +13,7 @@ public final class VehicleConstant {
 
 	public static final long MIL = 1_000_000;
 
-	private VehicleConstant() {
+	private VehicleInfoConstant() {
 		throw new BusinessException(ErrorCode.ILLEGAL_UTILITY_CLASS_ACCESS);
 	}
 }
--- monicar-emulator/src/main/java/org/emulator/device/infrastructure/external/command/vo/FixedVehicleInfo.java
+++ monicar-emulator/src/main/java/org/emulator/device/infrastructure/external/command/vo/FixedVehicleInfo.java
@@ -2,21 +2,16 @@ package org.emulator.device.infrastructure.external.command.vo;
 
 import lombok.Getter;
 
-import org.emulator.device.VehicleConstant;
+import org.emulator.config.ApplicationContextProvider;
+import org.emulator.device.VehicleInfoConstant;
+import org.emulator.device.VehicleInfoDynamic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-/**
- * 차량의 기본 데이터를 담는 VO
- *
- * @field mdn     차량 번호 - 차량을 식별하는 고유 번호
- * @field num	  차량 번호 - 번호판 번호
- * @field tid     터미널 아이디 - 터미널 장치의 고유 식별자
- * @field mid     제조사 아이디 - 제조사를 구분하는 식별자
- * @field pv      패킷 버전 - 데이터 패킷의 버전 정보
- * @field did     디바이스 아이디 - 설치된 디바이스의 고유 식별자
- */
+@Component
 @Getter
 public class FixedVehicleInfo {
-	private static final FixedVehicleInfo INSTANCE = new FixedVehicleInfo();
+	private static FixedVehicleInfo INSTANCE;
 
 	private final long mdn;
 	private final String num;
@@ -25,16 +20,21 @@ public class FixedVehicleInfo {
 	private final int pv;
 	private final long did;
 
-	private FixedVehicleInfo() {
-		this.mdn = VehicleConstant.VEHICLE_PRIMARY_KEY;
-		this.num = VehicleConstant.VEHICLE_NUMBER;
-		this.tid = VehicleConstant.TERMINAL_ID;
-		this.mid = VehicleConstant.MANUFACTURER_ID;
-		this.pv = VehicleConstant.PACKET_VERSION;
-		this.did = VehicleConstant.DEVICE_ID;
+	@Autowired
+	public FixedVehicleInfo(VehicleInfoDynamic vehicleInfoDynamic) {
+		this.mdn = vehicleInfoDynamic.vehicleMdn();
+		this.num = vehicleInfoDynamic.vehicleNumber();
+		this.tid = VehicleInfoConstant.TERMINAL_ID;
+		this.mid = VehicleInfoConstant.MANUFACTURER_ID;
+		this.pv = VehicleInfoConstant.PACKET_VERSION;
+		this.did = VehicleInfoConstant.DEVICE_ID;
+		INSTANCE = this;
 	}
 
 	public static FixedVehicleInfo getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = ApplicationContextProvider.getApplicationContext().getBean(FixedVehicleInfo.class);
+		}
 		return INSTANCE;
 	}
 }
