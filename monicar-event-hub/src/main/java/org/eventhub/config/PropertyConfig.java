23089d1ab15185736b4075daf80a520c7b8bbabc
tomatozil <tomatoziiilll@gmail.com>
2025-02-17 01:24:33 +0900

    fix: PropertyConfig dev에서만 쓰도록 어노테이션 추가

--- monicar-event-hub/src/main/java/org/eventhub/config/PropertyConfig.java
+++ monicar-event-hub/src/main/java/org/eventhub/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package org.eventhub.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
+@Profile("dev")
 @PropertySources(@PropertySource("classpath:properties/env.properties"))
 public class PropertyConfig {
 }
