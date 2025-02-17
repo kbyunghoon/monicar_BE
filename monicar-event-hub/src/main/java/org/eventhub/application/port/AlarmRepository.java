3ef9679fe55f2c5e2ea3ebbbc31ce880266517a2
tomatozil <tomatoziiilll@gmail.com>
2025-02-17 17:54:14 +0900

    fix: 예외 처리로 원래 트랜잭션에 영향 없도록 하기

--- monicar-event-hub/src/main/java/org/eventhub/application/port/AlarmRepository.java
+++ monicar-event-hub/src/main/java/org/eventhub/application/port/AlarmRepository.java
@@ -6,5 +6,7 @@ import org.eventhub.domain.Alarm;
 
 public interface AlarmRepository {
 	Optional<Alarm> findByVehicleId(Long vehicleId);
+
+	Optional<Alarm> findByVehicleIdAlarmRequired(Long vehicleId);
 	Long save(Long vehicleId);
 }
