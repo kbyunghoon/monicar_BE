383076c143528bd6b984b8c9d2e84a14930b7078
kbyunghoon <dev.kbhoon@gmail.com>
2025-04-29 21:29:22 +0900

    fix: 환경변수 파일은 로컬에서만 관리하도록 수정

--- monicar-rabbitmq-collector/src/main/java/org/rabbitmqcollector/config/PropertyConfig.java
+++ monicar-rabbitmq-collector/src/main/java/org/rabbitmqcollector/config/PropertyConfig.java
@@ -1,9 +1,11 @@
 package org.rabbitmqcollector.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
+@Profile("local")
 @Configuration
 @PropertySources(@PropertySource("classpath:properties/env.properties"))
 public class PropertyConfig {
